7ad83c460110268f79e8bd5235d38359aa56ad7e
Martin SangDo <martin.sangdo@gmail.com>
2018-03-31 21:58:36 +0700

    - Get video in detail ign.com

--- parse_page.py
+++ parse_page.py
@@ -1,15 +1,22 @@
 #http://lxml.de/lxmlhtml.html
 from lxml import html
+
+import json
 import requests
 
-page = requests.get('http://www.ign.com/videos', headers={'User-Agent': 'Mozilla/5.0'})
+# page = requests.get('http://www.ign.com/videos', headers={'User-Agent': 'Mozilla/5.0'})
+page = requests.get('http://www.ign.com/videos/2018/04/01/are-x-men-console-games-dead', headers={'User-Agent': 'Mozilla/5.0'})
+
 # print page.content
 tree = html.fromstring(page.content)
 
 # items = tree.xpath('//a[@class="grid_4 alpha"]/text()')
-hrefs = [a.attrib['href'] for a in tree.xpath('//a[@class="grid_4 alpha"]')]
+# src = [tag.attrib['data-settings'] for tag in tree.xpath('//div[@class="video-embed-content-v6"]')]
 
-print 'Items: ', hrefs
+tag = tree.find_class('video-embed-content-v6')
+j = json.loads(tag[0].attrib['data-settings'])
+
+print j['video']['assetsByHeight']
 
 
 # This is probably because of mod_security or some similar server security feature which blocks known spider/bot user agents (urllib uses something like python urllib/3.3.0, it's easily detected). Try setting a known browser user agent with:
