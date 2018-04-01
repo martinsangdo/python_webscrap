5f20ec904f8f1d2e674de5260776de79fd181ab1
Martin SangDo <martin.sangdo@gmail.com>
2018-04-01 11:48:00 +0700

    - select fields from db

--- const.py
+++ const.py
@@ -3,3 +3,8 @@ PART='contentDetails,snippet'
 PER_PAGE='10'
 PLAYLIST_ID='UUbu2SsF-Or3Rsn3NxqODImw'
 API_KEY = 'AIzaSyCbEOvBCOQrBl4xHaKoDaSguRxmC4RZUiE'
+#database
+HOSTNAME = 'localhost'
+USERNAME = 'root'
+PASSWORD = 'root'
+DATABASE = 'game_review'
--- get_latest_video_gamespot.py
+++ get_latest_video_gamespot.py
@@ -1,11 +1,20 @@
 #author: Martin SangDo 2018
 #get latest youtube videos from channel GameSpot
-#http://lxml.de/lxmlhtml.html
+#https://www.a2hosting.com/kb/developer-corner/mysql/connecting-to-mysql-using-python
+
 from lxml import html
 
 import requests
 import const
+import MySQLdb
 
+# Simple routine to run a query on a database and print the results:
+def doQuery( conn ) :
+    cur = conn.cursor()
+    cur.execute( "SELECT * FROM video_link" )
+    for row in cur:
+    	print(row[2])
+        
 #make API link
 url = 'https://www.googleapis.com/youtube/v3/playlistItems?part='+const.PART+'+&maxResults='+const.PER_PAGE+'&playlistId='+const.PLAYLIST_ID+'&key='+const.API_KEY
 #send request with fake browser (Avoid 403 error)
@@ -14,5 +23,9 @@ data = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 j = data.json()
 video_list=j['items']
 
-for video in video_list:
-	print video['snippet']['title']
+# for video in video_list:
+# 	print video['snippet']['title']
+	
+myConnection = MySQLdb.connect(host=const.HOSTNAME, user=const.USERNAME, passwd=const.PASSWORD, db=const.DATABASE )
+doQuery( myConnection )
+myConnection.close()
