e4aedc58b7671b629878b65982a5e2c68eb75275
Martin SangDo <martin.sangdo@gmail.com>
2018-07-31 00:50:55 +0700

    - update newsletter content

--- send_mail.py
+++ send_mail.py
@@ -9,12 +9,12 @@ from email.mime.text import MIMEText
 # me == my email address
 # you == recipient's email address
 me = "no-reply@blockbod.com"
-you = "dtsang012@yahoo.com"
+you = "rexycho@gmail.com"
 
 # Create message container - the correct MIME type is multipart/alternative.
 msg = MIMEMultipart('alternative')
 msg['Subject'] = "Your newsletter in this week"
-msg['From'] = me
+msg['From'] = 'Blockbod Newsletter'
 msg['To'] = you
 
 # Create the body of the message (a plain-text and an HTML version).
@@ -79,6 +79,21 @@ html = """\
 				<table>
 					<tr>
 						<td class="small" width="15%" style="vertical-align: top; padding-right:5px;">
+              <a href="http://blockbod.me:8073/blockchain-news/antonopoulos-crypto-success-is-not-measured-by-price-but-by-adoption"><img src="https://btcmanager.com/wp-content/uploads/2018/07/Antonopoulos-Crypto-Success-is-Not-Measured-by-Adoption-or-Market-Cap-768x458.jpg" style="width:75px;height:75px;"/></a>
+            </td>
+						<td>
+							<h6><a href="http://blockbod.me:8073/blockchain-news/antonopoulos-crypto-success-is-not-measured-by-price-but-by-adoption">Antonopoulos: Crypto Success Is not Measured by Price, but by Adoption</a></h6>
+							<p>Prominent Bitcoin and security expert, Andreas Antonopoulos, recently said in a talk presented at the University College Dublin in Ireland, that the success of crypto and blockchain can not be measured by price but must be measured by adoption.</p>
+						</td>
+					</tr>
+				</table>
+			</div><!-- /content -->
+
+      <!-- content -->
+			<div class="content">
+				<table>
+					<tr>
+						<td class="small" width="15%" style="vertical-align: top; padding-right:5px;">
               <a href="http://blockbod.com/blockchain-news/us-government-backs-dlt-based-energy-grid-with-1-million-grant"><img src="https://media.coindesk.com/uploads/2018/07/electric-grid-768x512.jpg" style="width:75px;height:75px;"/></a>
             </td>
 						<td>
@@ -89,41 +104,129 @@ html = """\
 				</table>
 			</div><!-- /content -->
 
+      <!-- content -->
+			<div class="content">
+				<table>
+					<tr>
+						<td class="small" width="15%" style="vertical-align: top; padding-right:5px;">
+              <a href="http://blockbod.me:8073/blockchain-news/12-billion-bitmains-second-major-investment-in-2018-is-eos"><img src="https://www.ccn.com/wp-content/uploads/2018/07/EOS-china-asia-imtoken-768x512.jpg" style="width:75px;height:75px;"/></a>
+            </td>
+						<td>
+							<h6><a href="http://blockbod.me:8073/blockchain-news/12-billion-bitmains-second-major-investment-in-2018-is-eos">$12 Billion Bitmain's Second Major Investment in 2018 is EOS</a></h6>
+							<p>Bitmain, the world's largest cryptocurrency company valued at $12 billion, and billionaire technology investor Peter Thiel have invested in Block.one, the development firm behind EOS.</p>
+						</td>
+					</tr>
+				</table>
+			</div><!-- /content -->
+
+      <!-- content -->
+			<div class="content">
+				<table>
+					<tr>
+						<td class="small" width="15%" style="vertical-align: top; padding-right:5px;">
+              <a href="http://blockbod.me:8073/blockchain-news/brazilian-police-bust-gang-who-used-bitcoin-to-launder-millions-of-dollars"><img src="https://www.ccn.com/wp-content/uploads/2018/03/Brazil-real-bitcoin-768x472.jpg" style="width:75px;height:75px;"/></a>
+            </td>
+						<td>
+							<h6><a href="http://blockbod.me:8073/blockchain-news/brazilian-police-bust-gang-who-used-bitcoin-to-launder-millions-of-dollars">Brazilian Police Bust Gang Who Used Bitcoin to Launder Millions of Dollars</a></h6>
+							<p>Brazil's Federal Police has recently arrested 12 individuals connected to a gang that allegedly used bitcoin to launder "millions of dollars" earned smuggling drugs into Europe, Asia, and Africa.</p>
+						</td>
+					</tr>
+				</table>
+			</div><!-- /content -->
+
+      <!-- content -->
+			<div class="content">
+				<table>
+					<tr>
+						<td class="small" width="15%" style="vertical-align: top; padding-right:5px;">
+              <a href="http://blockbod.me:8073/blockchain-news/brazilian-police-bust-gang-who-used-bitcoin-to-launder-millions-of-dollars"><img src="https://www.ccn.com/wp-content/uploads/2018/03/Brazil-real-bitcoin-768x472.jpg" style="width:75px;height:75px;"/></a>
+            </td>
+						<td>
+							<h6><a href="http://blockbod.me:8073/blockchain-news/brazilian-police-bust-gang-who-used-bitcoin-to-launder-millions-of-dollars">Brazilian Police Bust Gang Who Used Bitcoin to Launder Millions of Dollars</a></h6>
+							<p>Brazil's Federal Police has recently arrested 12 individuals connected to a gang that allegedly used bitcoin to launder "millions of dollars" earned smuggling drugs into Europe, Asia, and Africa.</p>
+						</td>
+					</tr>
+				</table>
+			</div><!-- /content -->
+
+      <!-- content -->
+      <div class="content">
+        <table>
+          <tr>
+            <td class="small" width="15%" style="vertical-align: top; padding-right:5px;">
+              <a href="http://blockbod.me:8073/blockchain-news/antonopoulos-crypto-success-is-not-measured-by-price-but-by-adoption"><img src="https://btcmanager.com/wp-content/uploads/2018/07/Antonopoulos-Crypto-Success-is-Not-Measured-by-Adoption-or-Market-Cap-768x458.jpg" style="width:75px;height:75px;"/></a>
+            </td>
+            <td>
+              <h6><a href="http://blockbod.me:8073/blockchain-news/antonopoulos-crypto-success-is-not-measured-by-price-but-by-adoption">Antonopoulos: Crypto Success Is not Measured by Price, but by Adoption</a></h6>
+              <p>Prominent Bitcoin and security expert, Andreas Antonopoulos, recently said in a talk presented at the University College Dublin in Ireland, that the success of crypto and blockchain can not be measured by price but must be measured by adoption.</p>
+            </td>
+          </tr>
+        </table>
+      </div><!-- /content -->
+
+      <!-- content -->
+      <div class="content">
+        <table>
+          <tr>
+            <td class="small" width="15%" style="vertical-align: top; padding-right:5px;">
+              <a href="http://blockbod.com/blockchain-news/us-government-backs-dlt-based-energy-grid-with-1-million-grant"><img src="https://media.coindesk.com/uploads/2018/07/electric-grid-768x512.jpg" style="width:75px;height:75px;"/></a>
+            </td>
+            <td>
+              <h6><a href="http://blockbod.com/blockchain-news/us-government-backs-dlt-based-energy-grid-with-1-million-grant">US Government Backs DLT-Based Energy Grid With $1 Million Grant</a></h6>
+              <p>The U.S. Department of Energy (DoE) has announced it will award a grant of nearly $1 million to a blockchain startup in an effort to advance the development of a decentralized energy grid infrastructure.</p>
+            </td>
+          </tr>
+        </table>
+      </div><!-- /content -->
+
+      <!-- content -->
+      <div class="content">
+        <table>
+          <tr>
+            <td class="small" width="15%" style="vertical-align: top; padding-right:5px;">
+              <a href="http://blockbod.me:8073/blockchain-news/12-billion-bitmains-second-major-investment-in-2018-is-eos"><img src="https://www.ccn.com/wp-content/uploads/2018/07/EOS-china-asia-imtoken-768x512.jpg" style="width:75px;height:75px;"/></a>
+            </td>
+            <td>
+              <h6><a href="http://blockbod.me:8073/blockchain-news/12-billion-bitmains-second-major-investment-in-2018-is-eos">$12 Billion Bitmain's Second Major Investment in 2018 is EOS</a></h6>
+              <p>Bitmain, the world's largest cryptocurrency company valued at $12 billion, and billionaire technology investor Peter Thiel have invested in Block.one, the development firm behind EOS.</p>
+            </td>
+          </tr>
+        </table>
+      </div><!-- /content -->
+
+      <!-- content -->
+      <div class="content">
+        <table>
+          <tr>
+            <td class="small" width="15%" style="vertical-align: top; padding-right:5px;">
+              <a href="http://blockbod.me:8073/blockchain-news/brazilian-police-bust-gang-who-used-bitcoin-to-launder-millions-of-dollars"><img src="https://www.ccn.com/wp-content/uploads/2018/03/Brazil-real-bitcoin-768x472.jpg" style="width:75px;height:75px;"/></a>
+            </td>
+            <td>
+              <h6><a href="http://blockbod.me:8073/blockchain-news/brazilian-police-bust-gang-who-used-bitcoin-to-launder-millions-of-dollars">Brazilian Police Bust Gang Who Used Bitcoin to Launder Millions of Dollars</a></h6>
+              <p>Brazil's Federal Police has recently arrested 12 individuals connected to a gang that allegedly used bitcoin to launder "millions of dollars" earned smuggling drugs into Europe, Asia, and Africa.</p>
+            </td>
+          </tr>
+        </table>
+      </div><!-- /content -->
+
 			<!-- content -->
 			<div class="content">
 				<table>
 					<tr>
 						<td>
 							<!-- social & contact -->
-							<table bgcolor="" class="social" width="100%">
+							<table class="social" width="100%">
 								<tr>
 									<td>
-
-										<!--- column 1 -->
-										<div class="column">
-											<table bgcolor="" cellpadding="" align="left">
-										<tr>
-											<td>
-
-												<h5 class="">Connect with Us:</h5>
-												<p class=""><a href="#" class="soc-btn fb">Facebook</a> <a href="#" class="soc-btn tw">Twitter</a> <a href="#" class="soc-btn gp">Google+</a></p>
-
-
-											</td>
-										</tr>
-									</table><!-- /column 1 -->
-										</div>
-
 										<!--- column 2 -->
 										<div class="column">
-											<table bgcolor="" cellpadding="" align="left">
+											<table align="left">
 										<tr>
 											<td>
 
-												<h5 class="">Contact Info:</h5>
-												<p>Phone: <strong>408.341.0600</strong><br/>
-                Email: <strong><a href="emailto:hseldon@trantor.com">hseldon@trantor.com</a></strong></p>
-
+												<h5>Contact Info:</h5>
+												<p>Website: <strong><a href="http://blockbod.com">http://blockbod.com</a></strong><br/>
+                            Email: <strong><a href="mailto:info@blockbod.com">info@blockbod.com</a></strong></p>
 											</td>
 										</tr>
 									</table><!-- /column 2 -->
@@ -151,16 +254,14 @@ html = """\
 	<tr>
 		<td></td>
 		<td class="container">
-
 				<!-- content -->
 				<div class="content">
 					<table>
 						<tr>
 							<td align="center">
 								<p>
-									<a href="#">Terms</a> |
-									<a href="#">Privacy</a> |
-									<a href="#"><unsubscribe>Unsubscribe</unsubscribe></a>
+									<a href="http://blockbod.com/publicapi/terms">Terms</a> |
+									<a href="http://blockbod.com/publicapi/privacy">Privacy</a>
 								</p>
 							</td>
 						</tr>
