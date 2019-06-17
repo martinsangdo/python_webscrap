5d65668b73734c48e24f189b1b1d8daae690d094
Martin SangDo <sang.do@knorex.com>
2019-06-17 17:08:36 +0700

    - get youtube video

--- parse_watchmojo_detail.py
+++ parse_watchmojo_detail.py
@@ -4,6 +4,10 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox') # required when running as root user. otherwise you would get no sandbox errors.
 driver = webdriver.Chrome(executable_path='/Users/sangdt/Downloads/SetupApps/python/chromedriver', options=chrome_options)
-driver.get('https://watchmojo.com/video/id/24126')
-text = driver.find_element_by_id('transcript')
-print(text.text)
+#dailymotion
+# driver.get('https://watchmojo.com/video/id/24126')
+# video = driver.find_element_by_class_name('resp-iframe')
+#youtube
+driver.get('https://watchmojo.com/video/id/28424')
+video = driver.find_element_by_xpath('//div[@class="scalable-content"]/iframe[1]')
+print(video.get_attribute('src'))
