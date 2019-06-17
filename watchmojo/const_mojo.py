3b9a18fd7cc4b245dc5154f6f8ae5a1c360ec8e5
Martin SangDo <sang.do@knorex.com>
2019-06-17 17:55:45 +0700

    - connect db

---
+++ watchmojo/const_mojo.py
@@ -0,0 +1,10 @@
+#constant
+# ln -s /Applications/MAMP/tmp/mysql/mysql.sock /tmp/mysql.sock
+HOSTNAME = 'localhost'
+USERNAME = 'root'
+PASSWORD = 'root'
+DATABASE = 'watchmojo'
+PORT = 8889
+#path to saved Chrome driver
+CHROME_DRIVER = '/Users/sangdt/Downloads/SetupApps/python/chromedriver'
+DOMAIN = 'https://watchmojo.com/'
---
+++ watchmojo/parse_watchmojo_cat.py
@@ -0,0 +1,46 @@
+#get all updated videos by category, not parse each video detail yet
+import const_mojo
+import mysql.connector
+from selenium import webdriver
+import sys
+
+def getCategoryInfo(conn, cat_id):
+	cur = conn.cursor()
+	sql = 'SELECT slug FROM tbl_category WHERE is_active=1 AND id='+cat_id
+	cur.execute(sql)
+	row = cur.fetchone()
+	return row
+
+# Option 1 - with ChromeOptions
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox') # required when running as root user. otherwise you would get no sandbox errors.
+driver = webdriver.Chrome(executable_path=const_mojo.CHROME_DRIVER, options=chrome_options)
+#get category id
+if (len(sys.argv) == 1):	#empty parameter
+	sys.exit()
+category_id = sys.argv[1]
+#connect db
+db_config = {
+  'user': const_mojo.USERNAME,
+  'password': const_mojo.PASSWORD,
+  'host': const_mojo.HOSTNAME,
+  'port': const_mojo.PORT,
+  'database': const_mojo.DATABASE,
+  'raise_on_warnings': True
+}
+
+cnx = mysql.connector.connect(**db_config)
+cat_info = getCategoryInfo(cnx, category_id)
+if (cat_info is None):
+    sys.exit()
+
+# begin parse page
+driver.get(const_mojo.DOMAIN+'categories/'+cat_info[0]+'/1')
+tags = []
+tags = driver.find_elements_by_class_name('owl-item')
+print (len(tags))
+# for tag in tags:
+#     print(tag.text)
+
+cnx.close()
