3a794ff1409883912f9f0d6dcf9f3a2000fb48d9
Martin SangDo <sang.do@knorex.com>
2019-06-17 16:44:40 +0700

    - analytic watchmojo

--- parse_watchmojo_cat.py
+++ parse_watchmojo_cat.py
@@ -4,10 +4,9 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox') # required when running as root user. otherwise you would get no sandbox errors.
 driver = webdriver.Chrome(executable_path='/Users/sangdt/Downloads/SetupApps/python/chromedriver', options=chrome_options)
-driver.get('https://watchmojo.com/video/id/28413')
-# tags = []
-# tags = driver.find_elements_by_class_name('owl-item')
+driver.get('https://watchmojo.com/categories/music/1')
+tags = []
+tags = driver.find_elements_by_class_name('owl-item')
+print (len(tags))
 # for tag in tags:
 #     print(tag.text)
-video = driver.find_element_by_id('videoplay')
-print(video)
--- parse_watchmojo_detail.py
+++ parse_watchmojo_detail.py
@@ -4,10 +4,6 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox') # required when running as root user. otherwise you would get no sandbox errors.
 driver = webdriver.Chrome(executable_path='/Users/sangdt/Downloads/SetupApps/python/chromedriver', options=chrome_options)
-driver.get('https://watchmojo.com/video/id/28413')
-# tags = []
-# tags = driver.find_elements_by_class_name('owl-item')
-# for tag in tags:
-#     print(tag.text)
-video = driver.find_element_by_id('videoplay')
-print(video)
+driver.get('https://watchmojo.com/video/id/24126')
+text = driver.find_element_by_id('transcript')
+print(text.text)
