2c9fe6fc30030e89f56fc2489fc1b1414a858ee2
Martin Sang Do <dothanhsang1984@yahoo.com>
2025-12-20 21:49:08 +0700

    - export and update based on _id

--- cert_exam/generate_questions.py
+++ cert_exam/generate_questions.py
@@ -37,7 +37,8 @@ MULTI_SELECTION_PROMPT = COMMON_QUESTION_PROMPT + os.environ['MULTI_SELECTION_PR
 
 # %%
 OPEN_ROUTER_AI_KEY=os.environ['OPENROUTER_KEY']
-#print(OPEN_ROUTER_AI_KEY)
+#platform = 'OPENROUTER'
+
 # %%
 def store_questions_2_db(platform, collection, raw_questions, question_type):
     questions = const.extract_questions_from_candidates(platform, raw_questions)
@@ -71,8 +72,10 @@ def generate_questions(cert_metadata, platform):
     if 'multi_choice_prompt_prefix' in cert_metadata:
         text_prompt = cert_metadata['multi_choice_prompt_prefix'].replace('{exam_name}', exam_name) + MULTI_CHOICE_PROMPT
         final_prompt = ROLE_PROMPT + context + text_prompt
-        # print('final_prompt', final_prompt)
-        # return
+        ################
+        print(final_prompt)
+        return
+        ################
         no_of_loop = ceil(cert_metadata['multi_choice_questions'] / 10)
         for i in range(no_of_loop):
             if platform is None or platform == '':
@@ -80,15 +83,12 @@ def generate_questions(cert_metadata, platform):
                 raw_generated_text = const.post_request_generative_ai(GENERATIVE_URI, final_prompt)
                 if 'error' in raw_generated_text and 'message' in raw_generated_text['error']:
                     if raw_generated_text['error']['message'].find('You exceeded your current quota') >= 0:
-                        print('Gemini: You exceeded your current quota, pls try other key or wait until next day')
+                        print('You exceeded your current quota, pls try other key or wait until next day')
                         exceeded_quota = True
                         break
             elif platform == 'OPENROUTER':
                 raw_generated_text = const.send_raw_request_2_openrouter(final_prompt, OPEN_ROUTER_AI_KEY)
-                if raw_generated_text.find('This request requires more credits') >= 0:
-                    print('OpenRouter: You exceeded your current quota, pls try other key or wait until next day')
-                    exceeded_quota = True
-                    break
+            
             store_questions_2_db(platform, question_collection, raw_generated_text, 'multiple-choice')
             time.sleep(5)   #delay 5 seconds
     #multi selection, if any
@@ -133,10 +133,12 @@ def export_csv(cert_metadata, test_set_number):
     manual_uuid = []
     #1. export multiple-choice first
     pipeline = [
-                {"$match": {'exported':0, 'type': 'multiple-choice'}},
+                {"$match": {'exported': {"$in": [0, None]}, 'type': 'multiple-choice'}},
                 {"$sample": {"size": cert_metadata['multi_choice_questions']}}
             ]
     random_documents = list(question_collection.aggregate(pipeline))
+    print('set', test_set_number)
+    print('len of questions', len(random_documents))
     for doc in random_documents:
         # print(doc)
         if 'D' not in doc['options']:
@@ -152,7 +154,7 @@ def export_csv(cert_metadata, test_set_number):
                                   '', #overall
                                   '' #domain
                                   ])
-        exported_uuid.append(doc['uuid'])
+        exported_uuid.append(doc['_id'])
     #2. multi selection
     if 'multi_selection_questions' in cert_metadata and cert_metadata['multi_selection_questions'] > 0:
         pipeline = [
@@ -161,8 +163,8 @@ def export_csv(cert_metadata, test_set_number):
                 ]
         random_documents = list(question_collection.aggregate(pipeline))
         for doc in random_documents:
-            exported_uuid.append(doc['uuid'])
-            manual_uuid.append(doc['uuid']) #they do not suppor bulk upload this type of question, we need to manually add them
+            exported_uuid.append(doc['_id'])
+            manual_uuid.append(doc['_id']) #they do not suppor bulk upload this type of question, we need to manually add them
     #save all questions to csv
     filename = cert_metadata['csv_filename_prefix']+test_set_number+'.csv'
     try:
@@ -171,16 +173,10 @@ def export_csv(cert_metadata, test_set_number):
             writer.writerows(file_data)
             print(f"Data successfully saved to '{file_path}{filename}'")
             for _id in exported_uuid:
-                question_collection.update_one({'uuid': _id}, {'$set': {'exported': 1, 'filename': filename}})
+                question_collection.update_one({'_id': _id}, {'$set': {'exported': 1, 'filename': filename}})
             print('","'.join(manual_uuid))
     except Exception as e:
         print(f"An error occurred while saving the array: {e}")
-
-# %%
-#run it: python generate_questions.py
-cert_symbol = 'SCRUM_PSD_1' #predefined in db (create new folder in this project in advance)
-platform = ''#'OPENROUTER'
-begin_generate_questions(cert_symbol, platform, 1)    #ideally 6 full tests
 
 # %%
 #export 1 test at once
@@ -193,74 +189,85 @@ def begin_export_csv(cert_symbol, test_set_number):
         return
     #
     export_csv(cert_metadata, test_set_number)
-    
+
+# %%
+#set flag to exams that have incorrect exported test
+def find_wrong_exams():
+    certs = metadata_collection.find({'udemy_link': {'$ne':None}})
+    for cert in certs:
+        question_collection = db[cert['collection_name']]
+        exported_questions = question_collection.find({'exported': 1})
+        if len(list(exported_questions)) < 240:
+            print(cert['symbol'])
+            print(cert['collection_name'])
+            print('========')
+#test
+find_wrong_exams()
+
+# %% [markdown]
+# 
+
+# %%
+#remove duplicated questions by UUID
+def remove_duplicated_questions_by_uuid(collection_name):
+    question_collection = db[collection_name]
+    questions = question_collection.find({})    #find all
+    uuid_map = {}   #duplicated uuids, key: uuid, value: 0/1 
+    for question in questions:
+        if question['uuid'] not in uuid_map:
+            #first appear
+            uuid_map[question['uuid']] = 1
+        else:
+            #this UUID is duplicated
+            uuid_map[question['uuid']] = uuid_map[question['uuid']] + 1
+    #
+    for uuid in uuid_map:
+        if uuid_map[uuid] > 1:
+            #remove duplicated question
+            question_collection.delete_many({'uuid':uuid})
+
+#test
+#remove_duplicated_questions_by_uuid('tb_az_az_500')
+
+# %%
+#remove duplicated questions by question
+def remove_duplicated_questions_by_question(collection_name):
+    question_collection = db[collection_name]
+    questions = question_collection.find({})    #find all
+    question_map = {}   #duplicated uuids, key: question, value: 0/1 
+    num_of_duplicated = 0
+    for question in questions:
+        if question['question'] not in question_map:
+            #first appear
+            question_map[question['question']] = 0
+        else:
+            #this UUID is duplicated
+            question_map[question['question']] = 1
+    #
+    for question in question_map:
+        if question_map[question] > 0:
+            num_of_duplicated = num_of_duplicated + 1
+    print('num_of_duplicated', num_of_duplicated)
+            
+    #remove duplicated question
+    # for question in question_map:
+    #     if question_map[question] > 0:
+    #         question_collection.delete_many({'question':question})
+
+#test
+remove_duplicated_questions_by_question('tb_terra_a_004')
+
+# %%
+#run it: python generate_questions.py
+cert_symbol = 'TERRA_A_004' #predefined in db (create new folder in this project in advance)
+platform = ''
+# begin_generate_questions(cert_symbol, platform, 1)    #ideally 6 full tests
+
 #generate CSV files
 # for i in range(1,7):  
 #     begin_export_csv(cert_symbol, str(i))    #Practice set index
 
-# %% [markdown]
-# <strong>template</strong>
-# 
-# https://medium.com/p/74e3eb52a9fe/edit
-# 
-# 15 New AWS Certified Solutions Architect - Associate (SAA-C03) Exam Questions You Need to Study - Dec 2025
-# 
-# These Practice Questions are Designed For:
-# 
-# - Those preparing for the AWS Certified Solutions Architect - Associate [SAA-C03] exam
-# 
-# - Individuals who prioritize passing the certification on the very first try
-# 
-# - Anyone wanting to test their AWS skills and identify areas for improvement before the actual exam
-# 
-# - Those seeking a practical way to learn AWS with comprehensive questions, answers, and references
-# 
-# - IT professionals who want to feel prepared and confident for AWS technical job interviews
-# 
-# - Anyone looking to boost their career trajectory and income through AWS Certification
-# 
-# - - -
-# 
-# Question 1:
-# 
-# Your application requires infrequent backups. Which S3 storage class is most cost-effective?
-# 
-# A. S3 Standard
-# 
-# B. S3 Intelligent-Tiering
-# 
-# C. S3 Glacier Flexible Retrieval
-# 
-# D. S3 One Zone-Infrequent Access
-# 
-# - - -
-# 
-# Check out this course for more practice questions!
-# 
-# - - -
-# 
-# Answers and explanations:
-# 
-# Question 1: A
-# 
-# A. S3 Standard
-# 
-# B. S3 Intelligent-Tiering
-# 
-# C. S3 Glacier Flexible Retrieval
-# 
-# D. S3 One Zone-Infrequent Access
-# 
-# - - -
-# 
-# Check out this site for more professional IT certification practice exams.
-# https://www.udemy.com/user/martindo/
-
 # %%
-#generate Medium page content
-#title (hard code): 15 New AWS Certified Solutions Architect - Associate (SAA-C03) Exam Questions You Need to Study - Dec 2025
-#introduction (from db blog_intro_template): replace \n
-#
 
 
 
