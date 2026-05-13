1915eb412e9a383f2898fe48ee7160de9e360b7d
martinsangdo <dothanhsang1984@yahoo.com>
2026-05-14 05:47:12 +0700

    - download & convert video format

--- yt_video/download_video.py
+++ yt_video/download_video.py
@@ -15,16 +15,24 @@ def download_youtube_clip(video_name, video_id, durations):
     #     "quiet": True
     # }``
 
-    ydl_opts = {
+    # ydl_opts = {
+    #     "format": "bestvideo*",
+    #     "outtmpl": f"{video_id}_temp.%(ext)s",
+    #     "quiet": True,
+    # }
+
+    with yt_dlp.YoutubeDL({
         "format": "bestvideo*",
         "outtmpl": f"{video_id}_temp.%(ext)s",
         "quiet": True,
-    }
+    }) as ydl:
+        info = ydl.extract_info(youtube_url, download=True)
+        temp_file = ydl.prepare_filename(info)
 
     print("Downloading video...")
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([youtube_url])
+    # with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    #     ydl.download([youtube_url])
 
     print("Cutting clip...")
 
@@ -34,14 +42,26 @@ def download_youtube_clip(video_name, video_id, durations):
         output_file = (
             f"{video_name}_{video_id}_{i}_{start_time.replace(':', '-')}_{end_time.replace(':', '-')}.mp4"
         )
+        # cmd = [
+        #     "ffmpeg",
+        #     "-y",
+        #     "-ss", start_time,
+        #     "-to", end_time,
+        #     "-i", temp_file ,
+        #     "-c", "copy",
+        #     "-an",
+        #     output_file
+        # ]
         cmd = [
             "ffmpeg",
             "-y",
             "-ss", start_time,
             "-to", end_time,
-            "-i", temp_file ,
-            "-c", "copy",
+            "-i", temp_file,
             "-an",
+            "-c:v", "libx264",
+            "-preset", "fast",
+            "-crf", "18",
             output_file
         ]
         subprocess.run(cmd, check=True)
@@ -57,16 +77,16 @@ def download_youtube_clip(video_name, video_id, durations):
 # Example usage
 
 download_youtube_clip(
-    video_name = "aws_vpc",
-    video_id="ApGz8tpNLgo",
+    video_name = "aws_aim",
+    video_id="hAk-7ImN6iM",
     durations = [
         {
-            "start_time" : "00:57",
-            "end_time": "02:57"
+            "start_time" : "01:19",
+            "end_time": "02:35"
         },
-        {   
-            "start_time" : "08:34",
-            "end_time": "09:49"
-        },
+        # {   
+        #     "start_time" : "08:34",
+        #     "end_time": "09:49"
+        # },
     ]
 )
